93eceee134b2ac0c81660ab5540cb586b3de3e1e
Alan Raso <alan_raso+github@yahoo.com.br>
2015-07-21 11:44:44 -0300

    Fixes
    Quebra do app arrumada

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,42 +1,34 @@
 
-var express 		  = require('express');
-var router  		  = express.Router();
-var login 			  = require('../models/login.js');
-var agendamento		= require('../models/agendamento.js');
+var express       = require('express');
+var router        = express.Router();
+var login         = require('../models/login.js');
+var agendamento   = require('../models/agendamento.js');
 var cliente       = require('../models/cliente.js');
 
 router.get('/', function(req, res) {
-	res.render('pages/login')
+  res.render('pages/login')
 });
 
 /**
 *  R O T A S   P A R A   A S   T E L A S   D E   L O G I N  /  L O G O U T
 */
-router.get('/agendamento', login.restrict, agendamento.doAllAgendamentos);
-
-/**
-*	Rota para a tela de Serviços e Peças
-*/
-
-router.get('/serv_pecas', function(req, res){
-	res.render('pages/serv_pecas')
+router.get('/login', function(req, res){
+  res.redirect('/');
 });
 
 /**
-*	Comando Login
+* R O T A S   P A R A   A S   T E L A S   D E   S E R V I Ç O S / P E Ç A S
 */
-router.get('/login', function(req, res){
-  res.redirect('/');
+router.get('/serv_pecas', function(req, res){
+  res.render('pages/serv_pecas')
 });
 
 router.post('/login', login.do_login);
 router.get('/logout', login.do_logout);
 /**
-*	 R O T A S   P A R A   A S   T E L A S   D E   A G E N D A M E N T O
+*  R O T A S   P A R A   A S   T E L A S   D E   A G E N D A M E N T O
 */
 router.get('/agendamento'       /*,login.restrict*/, agendamento.doAgendamento); 
 router.get('/agendamento/criar' ,login.restrict, agendamento.doCreateAgendamento);
-
-
 
 module.exports = router;
